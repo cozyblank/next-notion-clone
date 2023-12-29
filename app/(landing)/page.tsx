9667fdc7308949764c9321d6d05b6839bfeb35c8
jos50275266 <jos50275266@gmail.com>
2023-12-30 05:01:32 +0900

    04. heroes - [landing]

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Heading from "./_components/Heading";
+import Heroes from "./_components/Heroes";
 
 const LandingPage = () => {
   return (
     <div className="flex flex-col min-h-full">
       <div className="flex flex-col items-center justify-center flex-1 px-6 pb-10 text-center md:justify-start gap-y-8">
         <Heading />
+        <Heroes />
       </div>
     </div>
   );
