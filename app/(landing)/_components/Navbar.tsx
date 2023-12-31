090f0a3cd47164cb4476e45e3daf764ffaaedb60
jos50275266 <jos50275266@gmail.com>
2024-01-01 02:13:58 +0900

    06. navigation - [landing]

---
+++ app/(landing)/_components/Navbar.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { useScrollTrack } from "@/hooks/use-scroll-track";
+import { cn } from "@/lib/utils";
+import Logo from "./Logo";
+
+const Navbar = () => {
+  const scrolled = useScrollTrack();
+
+  return (
+    <div
+      className={cn(
+        "fixed top-0 z-50 flex items-center w-full p-6 bg-background",
+        scrolled && "border-b shadow-sm"
+      )}
+    >
+      <Logo />
+      <div className="flex items-center justify-between w-full md:ml-auto md:justify-end gap-x-2">
+        Login
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
