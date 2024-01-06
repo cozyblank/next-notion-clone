56ac38e760ae26903e60340c9d4038a0543a0488
jos50275266 <jos50275266@gmail.com>
2024-01-06 18:47:34 +0900

    12. Notion Sidebar 구현 #2 [사이드바 접고 펼치는 기능 탬플릿]

--- app/(service)/_components/Navigation.tsx
+++ app/(service)/_components/Navigation.tsx
@@ -1,13 +1,40 @@
 "use client";
 
+import { cn } from "@/lib/utils";
+import { ChevronsLeft, Menu, MenuIcon } from "lucide-react";
+import { usePathname } from "next/navigation";
+import { ElementRef, useRef, useState } from "react";
+import { useMediaQuery } from "usehooks-ts";
+
 const Navigation = () => {
+  const pathname = usePathname();
+  const isMobile = useMediaQuery("(max-width: 768px)");
+
+  const isResizingRef = useRef(false);
+  const sidebarRef = useRef<ElementRef<"aside">>(null);
+  const navbarRef = useRef<ElementRef<"div">>(null);
+  const [isResetting, setIsResetting] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(isMobile);
+
   return (
     <>
       <aside
-        className="h-full overflow-y-auto group bg-secondary
-      relative w-60 flex flex-col z-[99999]
-      "
+        ref={sidebarRef}
+        className={cn(
+          "h-full overflow-y-auto group bg-secondary relative w-60 flex flex-col z-[99999]",
+          isResetting && "transition-all ease-in-out duration-300",
+          isMobile && "w-0"
+        )}
       >
+        <div
+          role="button"
+          className={cn(
+            "absolute w-6 h-6 transition rounded-sm opacity-0 text-muted-foreground hover:bg-neutral-300 dark:hover:bg-neutral-600 top-3 right-2 group-hover:opacity-100",
+            isMobile && "opacity-100"
+          )}
+        >
+          <ChevronsLeft className="w-6 h-6" />
+        </div>
         <div>
           <p>Action</p>
         </div>
@@ -16,6 +43,20 @@ const Navigation = () => {
         </div>
         <div className="absolute top-0 right-0 w-1 h-full transition opacity-0 group-hover:opacity-100 cursor-ew-resize bg-primary/10" />
       </aside>
+      <div
+        ref={navbarRef}
+        className={cn(
+          "absolute top-0 left-60 z-[99999] w-[calc(100% - 240px)]",
+          isResetting && "transition-all ease-in-out",
+          isMobile && "left-0 w-full"
+        )}
+      >
+        <nav className="w-full px-3 py-2 bg-transparent">
+          {isCollapsed && (
+            <MenuIcon role="button" className="w-6 h-6 text-muted-foreground" />
+          )}
+        </nav>
+      </div>
     </>
   );
 };
