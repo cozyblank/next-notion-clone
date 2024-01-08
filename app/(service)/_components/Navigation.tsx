7c28ab8e60692383ff92972c25690009f2ba2176
jos50275266 <jos50275266@gmail.com>
2024-01-09 03:15:50 +0900

    20. Notion Sidebar - 기능 구현에 필요한 프로퍼티 설정

--- app/(service)/_components/Navigation.tsx
+++ app/(service)/_components/Navigation.tsx
@@ -1,7 +1,14 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { ChevronsLeft, Menu, MenuIcon, PlusCircle } from "lucide-react";
+import {
+  ChevronsLeft,
+  Menu,
+  MenuIcon,
+  PlusCircle,
+  Search,
+  Settings,
+} from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
@@ -131,6 +138,8 @@ const Navigation = () => {
         </div>
         <div>
           <UserItem />
+          <Item onClick={() => {}} label="Search" icon={Search} isSearch />
+          <Item onClick={() => {}} label="Settings" icon={Settings} />
           <Item onClick={handleCreate} label="New Document" icon={PlusCircle} />
         </div>
         <div className="mt-4">
