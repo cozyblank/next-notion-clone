bb5a2191a12ad4c446a4829ccec8c69ae2d9e354
jos50275266 <jos50275266@gmail.com>
2024-01-10 19:54:53 +0900

    24. Notion Document Archiving UI 구축 및 적용

--- app/(service)/_components/Item.tsx
+++ app/(service)/_components/Item.tsx
@@ -1,15 +1,25 @@
 "use client";
 
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
 import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
+import { useUser } from "@clerk/clerk-react";
 import { useMutation } from "convex/react";
 import {
   ChevronDown,
   ChevronRight,
   LucideIcon,
+  MoreHorizontal,
   Plus,
+  Trash,
 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import React from "react";
@@ -42,8 +52,27 @@ const Item = ({
 }: ItemProps) => {
   const router = useRouter();
   const create = useMutation(api.documents.create);
+  const archive = useMutation(api.documents.archive);
+  const { user } = useUser();
 
   const ChevronIcon = expanded ? ChevronDown : ChevronRight;
+
+  const onArchive = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    event.stopPropagation();
+    if (!id) return;
+
+    const promise = archive({ id }).then(() =>
+      router.push("/documents")
+    );
+
+    toast.promise(promise, {
+      loading: "Moving to trash...",
+      success: "Document moved to trash!!!",
+      error: "Failed to archive document...",
+    });
+  };
 
   const onCreate = (
     event: React.MouseEvent<HTMLDivElement, MouseEvent>
@@ -113,6 +142,32 @@ const Item = ({
 
       {!!id && (
         <div className="flex items-center ml-auto gap-x-2">
+          <DropdownMenu>
+            <DropdownMenuTrigger
+              onClick={(e) => e.stopPropagation()}
+              asChild>
+              <div
+                role="button"
+                className="h-full ml-auto rounded-sm opacity-0 group-hover:opacity-100 hover:bg-neutral-300 dark:hover:bg-neutral-600">
+                <MoreHorizontal className="w-4 h-4 text-muted-foreground" />
+              </div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              className="w-60"
+              align="start"
+              side="right"
+              forceMount>
+              <DropdownMenuItem onClick={onArchive}>
+                <Trash className="w-4 h-4 mr-2" />
+                Delete
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <div className="p-2 text-xs text-muted-foreground">
+                Last edited by: ${user?.fullName}
+              </div>
+            </DropdownMenuContent>
+          </DropdownMenu>
+
           <div
             role="button"
             onClick={onCreate}
