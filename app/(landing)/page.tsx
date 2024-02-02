b049785f6378075dd7c796e71af663ff895d5641
jos50275266 <jos50275266@gmail.com>
2024-02-03 03:05:51 +0900

    padding modified

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -5,7 +5,7 @@ import Footer from "./_components/Footer";
 const LandingPage = () => {
   return (
     <div className="flex flex-col min-h-full">
-      <div className="flex flex-col items-center justify-center flex-1 px-6 pb-10 text-center md:justify-start gap-y-8">
+      <div className="flex flex-col items-center justify-center flex-1 px-6 text-center md:justify-start gap-y-8">
         <Heading />
         <Heroes />
       </div>
